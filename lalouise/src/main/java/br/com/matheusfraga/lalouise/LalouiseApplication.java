626ffd73dbf5f153f212a6c8b8bc25b85a4fd923
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-04-29 16:47:14 -0300

    Subindo projeto backend

---
+++ lalouise/src/main/java/br/com/matheusfraga/lalouise/LalouiseApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusfraga.lalouise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalouiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalouiseApplication.class, args);
+	}
+
+}
---
+++ lalouise/src/test/java/br/com/matheusfraga/lalouise/LalouiseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusfraga.lalouise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalouiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
