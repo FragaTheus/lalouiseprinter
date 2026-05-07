6c4b2619b9bc7a44cf4a4da1359744e797c53356
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-05-07 00:09:47 -0300

    feat/managers: Finalizando funcionalidade de restaurates com testes unitario.

---
+++ lalouise/src/main/java/br/com/matheusfragadev/lalouise/domain/user/staff/entity/Manager.java
@@ -0,0 +1,23 @@
+package br.com.matheusfragadev.lalouise.domain.user.staff.entity;
+
+import br.com.matheusfragadev.lalouise.domain.user.credentials.enums.Role;
+import br.com.matheusfragadev.lalouise.domain.user.credentials.vo.Email;
+import br.com.matheusfragadev.lalouise.domain.user.credentials.vo.Nickname;
+import br.com.matheusfragadev.lalouise.domain.user.credentials.vo.Password;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "managers")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Manager extends Staff {
+
+    public Manager(Nickname nickname, Email email, Password password, UUID restaurantId) {
+        super(nickname, email, password, Role.MANAGER, restaurantId);
+    }
+}
+
